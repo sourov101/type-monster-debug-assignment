374eb6692bed013838dbd06f34ee8e16598ef872
sourov <sourov.jsr98@gmail.com>
2022-09-14 23:08:17 +0600

    mistake count fixed

--- script.js
+++ script.js
@@ -46,6 +46,8 @@ const typeController = (e) => {
     display.innerHTML += `<span class="green">${newLetter === " " ? "▪" : newLetter}</span>`;
   } else {
     display.innerHTML += `<span class="red">${newLetter === " " ? "▪" : newLetter}</span>`;
+    return errorCount++;
+
   }
 
   // check if given question text is equal to user typed text
@@ -67,7 +69,7 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTaken = parseInt((finishTime - startTime) / 1000);
 
   // show result modal
   resultModal.innerHTML = "";
